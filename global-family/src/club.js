9c206cf097bfff15397c0a725d7dea7880092312
isha-r <68084229+isha-r@users.noreply.github.com>
2023-10-06 18:56:54 -0500

    adding changes to check if club list works

--- global-family/src/club.js
+++ global-family/src/club.js
@@ -9,6 +9,30 @@ class App extends Component {
     };
   }
 
+  addClub = () => {
+    const newClub = {
+      name: 'Club Name',
+      description: 'Club Description',
+      mentor_id: 1, // Replace with the ID of the mentor
+      // Add other club-related data here
+    };
+
+    fetch('/api/clubs/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newClub),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data); // Log the response from the server
+      })
+      .catch((error) => {
+        console.error('Error adding club:', error);
+      });
+  };
+
   componentDidMount() {
     // Fetch the list of clubs from your Flask API
     fetch('/api/clubs')
@@ -26,6 +50,7 @@ class App extends Component {
     return (
       <div>
         <h1>Club List</h1>
+        <button onClick={this.addClub}>Add Club</button>
         <ul>
           {clubs.map((club) => (
             <li key={club.id}>{club.name}</li>
