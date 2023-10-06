839e25822fcb8a7c3fe30f030567bd9348808659
isha-r <68084229+isha-r@users.noreply.github.com>
2023-10-06 18:39:02 -0500

    added intiial app.js and new app.py

--- global-family/src/App.js
+++ global-family/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      clubs: [], // Initialize an empty array to store the list of clubs
+    };
+  }
+
+  componentDidMount() {
+    // Fetch the list of clubs from your Flask API
+    fetch('/api/clubs')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ clubs: data }); // Update the state with the fetched clubs
+      })
+      .catch((error) => {
+        console.error('Error fetching clubs:', error);
+      });
+  }
+
+  render() {
+    const { clubs } = this.state;
+    return (
+      <div>
+        <h1>Club List</h1>
+        <ul>
+          {clubs.map((club) => (
+            <li key={club.id}>{club.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ global-family/src/app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'  # Replace with your database URI
+db = SQLAlchemy(app)
+
+# Define Club and Event models
+class Club(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    mentor_id = db.Column(db.Integer, nullable=False)
+    # Add other club-related fields here
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    club_id = db.Column(db.Integer, nullable=False)
+    # Add other event-related fields here
+
+# API routes for clubs
+@app.route('/api/clubs', methods=['GET'])
+def get_clubs():
+    clubs = Club.query.all()
+    club_list = [{"id": club.id, "name": club.name, "description": club.description} for club in clubs]
+    return jsonify(club_list)
+
+@app.route('/api/clubs/<int:club_id>', methods=['GET'])
+def get_club(club_id):
+    club = Club.query.get_or_404(club_id)
+    club_data = {
+        "id": club.id,
+        "name": club.name,
+        "description": club.description,
+        "mentor_id": club.mentor_id
+        # Add other club-related data here
+    }
+    return jsonify(club_data)
+
+@app.route('/api/clubs', methods=['POST'])
+def create_club():
+    data = request.get_json()
+    new_club = Club(
+        name=data['name'],
+        description=data['description'],
+        mentor_id=data['mentor_id']
+        # Add other club-related data here
+    )
+    db.session.add(new_club)
+    db.session.commit()
+    return jsonify({"message": "Club created successfully."})
+
+# API routes for events (similar structure as clubs)
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
