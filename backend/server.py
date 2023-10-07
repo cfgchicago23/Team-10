f564c5d2cdf0797d609212778bd839feb3d2565f
Becky Blake <ryblake2@illinois.edu>
2023-10-07 01:20:57 -0500

    comment out content in server.py

--- backend/server.py
+++ backend/server.py
@@ -47,8 +47,8 @@ def signup():
     return 200
 
 
-@app.route("/content", methods=["GET"])
-def content():
+# @app.route("/content", methods=["GET"])
+# def content():
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000, host="0.0.0.0")
