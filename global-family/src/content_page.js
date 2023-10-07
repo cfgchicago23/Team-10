03c0397ee13c192094c6568766ccd176bc94ebc5
Ali Zia <ziaali455@gmail.com>
2023-10-06 20:23:12 -0500

    added custom tile texts

--- global-family/src/content_page.js
+++ global-family/src/content_page.js
@@ -1,27 +1,29 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const Tile = ({ id, onClick, isActive }) => {
-  const tileClassName = isActive ? 'tile active' : 'tile';
-  return <div className={tileClassName} onClick={() => onClick(id)} />;
+const Tile = ({ id, onClick, isActive, text }) => {
+  const tileClassName = isActive ? 'tile active' : 'tile';  
+  return <div className={tileClassName} onClick={() => onClick(id)}>
+    <p>{text}</p>
+  </div>;
 };
+
 
 const ContentPage = () => {
   const [activeTiles, setActiveTiles] = useState([]);
 
   const handleClick = (id) => {
-    setActiveTiles((prevActiveTiles) =>
-      prevActiveTiles.includes(id)
-        ? prevActiveTiles.filter((tileId) => tileId !== id)
-        : [...prevActiveTiles, id]
-    );
+    console.log("hi");
+    //display the respective menu
   };
 
   const renderTiles = () => {
     const tiles = [];
-    for (let i = 0; i < 16; i++) {
+    const titles = ["Trafficking", "Wellness", "Violence Response", "Self-Love"]
+    for (let i = 0; i < 4; i++) {
       const isActive = activeTiles.includes(i);
-      tiles.push(<Tile key={i} id={i} onClick={handleClick} isActive={isActive} />);
+      tiles.push(<Tile key={i} id={i} onClick={handleClick} isActive={isActive} text={titles[i]}>
+      </Tile>);
     }
     return tiles;
   };
