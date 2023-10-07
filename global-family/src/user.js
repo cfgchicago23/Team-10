f54c4f95e6db15a8a5b503d7b8761aefadafa54e
Rohan <rohshah@umich.edu>
2023-10-07 00:10:24 -0500

    Changed a lot to user.js, created more UI/UX features that has a search for clubs and created a feature that allows to search by country as well

--- global-family/src/user.js
+++ global-family/src/user.js
@@ -1,82 +1,105 @@
 import React, { Component } from 'react';
+import { Button, TextField, List, ListItem, ListItemText, ListItemSecondaryAction, Container, Typography, Select, MenuItem } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 
 class UserPage extends Component {
     state = {
-        clubs: [{id: 1, name: "Test Club"}],
-        joinedClubs: [{id: 1, name: "Test Club"}],
-        showJoinedClubs: false,
-        userId: 1
+        clubs: [{id: 1, name: "Test Club", country: "USA"}], // Added country for demonstration
+        joinedClubs: [],
+        userId: 1,
+        searchName: '',
+        searchCountry: ''
     }
 
+    // ... other methods ...
 
-    fetchAllClubs = () => {
-        fetch('/api/clubs')
-            .then(response => response.json())
-            .then(data => {
-                console.log("Fetched clubs:", data);  // <-- Add this
-                this.setState({ clubs: data });
-            });
+    handleSearchNameChange = (e) => {
+        this.setState({ searchName: e.target.value });
     }
 
-    handleJoinClub = (clubId) => {
-        fetch('/api/join_club', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                user_id: this.state.userId,
-                club_id: clubId,
-            }),
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.message === "Club joined successfully.") {
-                const clubToJoin = this.state.clubs.find(club => club.id === clubId);
-                this.setState(prevState => ({
-                    joinedClubs: [...prevState.joinedClubs, clubToJoin]
-                }));
-            }
-        });
+    handleSearchCountryChange = (e) => {
+        this.setState({ searchCountry: e.target.value });
     }
 
-
-    toggleJoinedClubs = () => {
-        this.setState(prevState => ({
-            showJoinedClubs: !prevState.showJoinedClubs
-        }));
-    }
-    componentDidMount() {
-        console.log("Component did mount");
-        this.fetchAllClubs();
+    filteredClubs = () => {
+        const { clubs, searchName, searchCountry } = this.state;
+        return clubs.filter(club => 
+            club.name.toLowerCase().includes(searchName.toLowerCase()) &&
+            (searchCountry === '' || club.country === searchCountry)
+        );
     }
 
     render() {
-        console.log("Rendering with state:", this.state);
+        const filteredClubs = this.filteredClubs();
         return (
-            <div>
-                <h2>Clubs</h2>
-                <ul>
-                    {this.state.clubs.map(club => (
-                        <li key={club.id}>
-                            {club.name}
-                            <button onClick={() => this.handleJoinClub(club.id)}>Add</button>
-                        </li>
-                    ))}
-                </ul>
+            <Container maxWidth="md" style={{ marginTop: '40px' }}>
+                <Typography variant="h4" gutterBottom>
+                    Clubs
+                </Typography>
+                <TextField 
+                    fullWidth
+                    variant="outlined"
+                    placeholder="Search clubs by name..."
+                    value={this.state.searchName}
+                    onChange={this.handleSearchNameChange}
+                    style={{ marginBottom: '20px' }}
+                />
+                <Select
+                    fullWidth
+                    value={this.state.searchCountry}
+                    onChange={this.handleSearchCountryChange}
+                    variant="outlined"
+                    style={{ marginBottom: '20px' }}
+                >
+                    <MenuItem value=""><em>Filter by Country</em></MenuItem>
+                    <MenuItem value="USA">USA</MenuItem>
+                    <MenuItem value="Canada">Canada</MenuItem>
+                    <MenuItem value="UK">UK</MenuItem>
+                    {/* Add more countries as needed */}
+                </Select>
+                <List>
+                    {filteredClubs.length === 0 ? (
+                        <ListItem>
+                            <ListItemText primary="No clubs found." />
+                        </ListItem>
+                    ) : (
+                        filteredClubs.map(club => (
+                            <ListItem key={club.id}>
+                                <ListItemText primary={club.name} secondary={club.country} />
+                                <ListItemSecondaryAction>
+                                    <Button 
+                                        variant="contained" 
+                                        color="primary" 
+                                        startIcon={<AddIcon />}
+                                        onClick={() => this.handleJoinClub(club.id)}
+                                    >
+                                        Add
+                                    </Button>
+                                </ListItemSecondaryAction>
+                            </ListItem>
+                        ))
+                    )}
+                </List>
 
-                <h2>Joined Clubs</h2>
-                <ul>
-                    {this.state.joinedClubs.map(club => (
-                        <li key={club.id}>{club.name}</li>
-                    ))}
-                </ul>
-            </div>
+                <Typography variant="h4" gutterBottom style={{ marginTop: '40px' }}>
+                    Joined Clubs
+                </Typography>
+                <List>
+                    {this.state.joinedClubs.length === 0 ? (
+                        <ListItem>
+                            <ListItemText primary="You haven't joined any clubs yet." />
+                        </ListItem>
+                    ) : (
+                        this.state.joinedClubs.map(club => (
+                            <ListItem key={club.id}>
+                                <ListItemText primary={club.name} secondary={club.country} />
+                            </ListItem>
+                        ))
+                    )}
+                </List>
+            </Container>
         );
     }
 }
 
 export default UserPage;
-
-
-
