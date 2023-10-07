b24af63033f25f82a313c41181fb093b6d878cb5
Rittvic <rittvicr@gmail.com>
2023-10-06 19:58:35 -0500

    add /signup endpoint and set up mongodb

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,48 @@
 from flask import Flask, jsonify, request
+from pymongo import MongoClient
+import bcrypt
+import uuid
 
 app = Flask(__name__)
+db_client = MongoClient('localhost', 27017)
+db = db_client.flask_db
+
+# set up mongodb collections
+user_accounts = db.user_accounts
+clubs = db.clubs
+
 
 @app.route("/signup", methods=["POST"])
 def signup():
+    """
+    Registers a new user to the system
+    :return: 200 if user successfully added, 400 otherwise
+    """
+    user_data = request.get_json()
+
+    # parse JSON data
+    user_email = user_data["email"]
+    user_password = user_data["password"]
+
+    # generate UUID for user id
+    user_id = uuid.uuid4()
+
+    # generate hashed (+salted) password
+    hashed_password = bcrypt.hashpw(user_password, bcrypt.gensalt())
+
+    # create document and insert into collection
+    user_document = {
+        "id": user_id,
+        "email": user_email,
+        "password": hashed_password
+    }
+
+    # validate that it is not a duplicate email
+    if user_accounts.find_one({'email': user_email}) is not None:
+        return jsonify({"error": "you can only have one account per email"}), 400
+
+    user_accounts.insert_one(user_document)
+    return 200
 
 
 if __name__ == "__main__":
