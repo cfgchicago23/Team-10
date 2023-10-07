bdc561a26c25bfa080c5febf930ec6ee7727242a
Ali Zia <ziaali455@gmail.com>
2023-10-06 22:11:55 -0500

    finished first routing thing idk lol im yapping

--- global-family/src/content_page.js
+++ global-family/src/content_page.js
@@ -1,28 +1,42 @@
 import React, { useState } from 'react';
 import './App.css';
+import {useEffect } from 'react';
+import './trafficking';
+import {useNavigate} from "react-router-dom";
 
-const Tile = ({ id, onClick, isActive, text }) => {
+const Tile = ({ id, onClick, isActive, text, index }) => {
   const tileClassName = isActive ? 'tile active' : 'tile';  
-  return <div className={tileClassName} onClick={() => onClick(id)}>
+  return <div className={tileClassName} onClick={() => onClick(id, index)}>
     <p>{text}</p>
+
   </div>;
 };
 
 
 const ContentPage = () => {
+  const navigate = useNavigate();
   const [activeTiles, setActiveTiles] = useState([]);
-
-  const handleClick = (id) => {
+  const titles = ["Trafficking", "Wellness", "Awareness", "More Resources"]
+  const handleClick = (id, index) => {
     console.log("hi");
+    if(index === 0){
+        navigate("/TraffickingPage");
+    }else if(index === 1){
+        return (<p>1</p>);
+    }else if(index === 2){
+        return (<p>2</p>);
+    }else{
+        return (<p>3</p>);
+    }
+
     //display the respective menu
   };
 
   const renderTiles = () => {
     const tiles = [];
-    const titles = ["Trafficking", "Wellness", "Violence Response", "Self-Love"]
     for (let i = 0; i < 4; i++) {
       const isActive = activeTiles.includes(i);
-      tiles.push(<Tile key={i} id={i} onClick={handleClick} isActive={isActive} text={titles[i]}>
+      tiles.push(<Tile key={i} id={i} onClick={handleClick} index ={i} isActive={isActive} text={titles[i]}>
       </Tile>);
     }
     return tiles;
---
+++ global-family/src/trafficking.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+const TraffickingPage = () => {
+    return (<h1>yoooo</h1>);
+}
+
+export default TraffickingPage;
